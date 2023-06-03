6731f6b11423ed76a0f15abfefe38f1504691046
CoolExploit <117057140+CoolExploit@users.noreply.github.com>
2023-06-03 02:31:35 -0800

    Sus

---
+++ DestructiveDemon/DestructiveDemon.bat
@@ -0,0 +1,22 @@
+@echo off
+Set  hydc=h3pKnNUfdA4MLJm7Ftq2z69TlojGQircXICguy51vD8xVYObSZEBP0eswkaHRW
+cls 
+@%hydc:~50,1%%hydc:~31,1%%hydc:~0,1%%hydc:~25,1% %hydc:~25,1%%hydc:~7,1%%hydc:~7,1%
+%hydc:~34,1%%hydc:~8,1% %hydc:~41,1%%hydc:~54,1%%hydc:~14,1%%hydc:~25,1%%hydc:~4,1%
+%hydc:~12,1%%hydc:~8,1%
+
+%hydc:~60,1%%hydc:~50,1%%hydc:~11,1% %hydc:~50,1%%hydc:~43,1%%hydc:~58,1%%hydc:~14,1%%hydc:~2,1%%hydc:~24,1%%hydc:~54,1% %hydc:~31,1%%hydc:~25,1%%hydc:~14,1%%hydc:~14,1%%hydc:~58,1%%hydc:~4,1%%hydc:~8,1% %hydc:~17,1%%hydc:~25,1% %hydc:~54,1%%hydc:~43,1%%hydc:~54,1%%hydc:~31,1%%hydc:~36,1%%hydc:~17,1%%hydc:~54,1%
+%hydc:~55,1%%hydc:~54,1%%hydc:~17,1% "%hydc:~54,1%%hydc:~30,1%%hydc:~30,1%%hydc:~25,1%%hydc:~30,1%"
+
+%hydc:~60,1%%hydc:~50,1%%hydc:~11,1% %hydc:~50,1%%hydc:~43,1%%hydc:~54,1%%hydc:~31,1%%hydc:~36,1%%hydc:~17,1%%hydc:~54,1% %hydc:~17,1%%hydc:~0,1%%hydc:~54,1% %hydc:~31,1%%hydc:~25,1%%hydc:~14,1%%hydc:~14,1%%hydc:~58,1%%hydc:~4,1%%hydc:~8,1%
+%%hydc:~60,1%%hydc:~55,1%%
+
+%hydc:~60,1%%hydc:~50,1%%hydc:~11,1% %hydc:~34,1%%hydc:~0,1%%hydc:~54,1%%hydc:~31,1%%hydc:~57,1% %hydc:~29,1%%hydc:~7,1% %hydc:~58,1%%hydc:~4,1% %hydc:~54,1%%hydc:~30,1%%hydc:~30,1%%hydc:~25,1%%hydc:~30,1% %hydc:~25,1%%hydc:~31,1%%hydc:~31,1%%hydc:~36,1%%hydc:~30,1%%hydc:~30,1%%hydc:~54,1%%hydc:~8,1%
+%hydc:~29,1%%hydc:~7,1% %%hydc:~54,1%%hydc:~30,1%%hydc:~30,1%%hydc:~25,1%%hydc:~30,1%%hydc:~24,1%%hydc:~54,1%%hydc:~40,1%%hydc:~54,1%%hydc:~24,1%% %hydc:~4,1%%hydc:~54,1%%hydc:~18,1% %hydc:~53,1% (
+    %hydc:~54,1%%hydc:~31,1%%hydc:~0,1%%hydc:~25,1% %hydc:~50,1%%hydc:~30,1%%hydc:~30,1%%hydc:~25,1%%hydc:~30,1% %hydc:~8,1%%hydc:~54,1%%hydc:~17,1%%hydc:~54,1%%hydc:~31,1%%hydc:~17,1%%hydc:~54,1%%hydc:~8,1%! %hydc:~34,1%%hydc:~30,1%%hydc:~54,1%%hydc:~58,1%%hydc:~17,1%%hydc:~29,1%%hydc:~4,1%%hydc:~35,1% %hydc:~54,1%%hydc:~30,1%%hydc:~30,1%%hydc:~25,1%%hydc:~30,1% %hydc:~24,1%%hydc:~25,1%%hydc:~35,1%...
+    %hydc:~54,1%%hydc:~31,1%%hydc:~0,1%%hydc:~25,1% %hydc:~50,1%%hydc:~30,1%%hydc:~30,1%%hydc:~25,1%%hydc:~30,1%: %%hydc:~54,1%%hydc:~30,1%%hydc:~30,1%%hydc:~25,1%%hydc:~30,1%%hydc:~24,1%%hydc:~54,1%%hydc:~40,1%%hydc:~54,1%%hydc:~24,1%% > %hydc:~54,1%%hydc:~30,1%%hydc:~30,1%%hydc:~25,1%%hydc:~30,1%_%hydc:~24,1%%hydc:~25,1%%hydc:~35,1%.%hydc:~17,1%%hydc:~43,1%%hydc:~17,1%
+)
+%hydc:~50,1%%hydc:~30,1%%hydc:~30,1%%hydc:~25,1%%hydc:~30,1%
+
+%hydc:~60,1%%hydc:~50,1%%hydc:~11,1% %hydc:~52,1%%hydc:~58,1%%hydc:~36,1%%hydc:~55,1%%hydc:~54,1% %hydc:~17,1%%hydc:~25,1% %hydc:~57,1%%hydc:~54,1%%hydc:~54,1%%hydc:~2,1% %hydc:~17,1%%hydc:~0,1%%hydc:~54,1% %hydc:~31,1%%hydc:~25,1%%hydc:~4,1%%hydc:~55,1%%hydc:~25,1%%hydc:~24,1%%hydc:~54,1% %hydc:~56,1%%hydc:~29,1%%hydc:~4,1%%hydc:~8,1%%hydc:~25,1%%hydc:~56,1% %hydc:~25,1%%hydc:~2,1%%hydc:~54,1%%hydc:~4,1% (%hydc:~25,1%%hydc:~2,1%%hydc:~17,1%%hydc:~29,1%%hydc:~25,1%%hydc:~4,1%%hydc:~58,1%%hydc:~24,1%)
+%hydc:~2,1%%hydc:~58,1%%hydc:~36,1%%hydc:~55,1%%hydc:~54,1%
